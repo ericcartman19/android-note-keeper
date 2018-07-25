00e2846a8cea18ea80c83099f5de00d901c12b56
benito <jlbeny100@gmail.com>
2018-07-25 13:49:44 +0200

    add method initializeComponenets()

--- app/src/main/java/com/demo/valoyes/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/demo/valoyes/notekeeper/NoteListActivity.java
@@ -1,11 +1,16 @@
 package com.demo.valoyes.notekeeper;
 
+import android.opengl.EGLExt;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
@@ -24,6 +29,21 @@ public class NoteListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initializeDisplayContent();
+    }
+
+    // popula nuestro listView de una manera muy similar a la manera como el spinner se asociaba a
+    // los cursos
+    // luego trataremos el user-selection, es decir cuando el usuario haga click en una note
+    // quereemos lanzar nuestro NoteActivity la cual va a mostrar la note en cuestion
+    private void initializeDisplayContent() {
+        ListView listNotess = (ListView) findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes =  DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        listNotess.setAdapter(adapterNotes);
     }
 
 }
