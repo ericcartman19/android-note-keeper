97c114410b850a9ea958dfd7d3928e3cd7d3d5f6
benito <jlbeny100@gmail.com>
2018-11-07 16:42:08 +0100

    add interaction with the spinner and retrieving information from the AdapterView

--- app/src/androidTest/java/com/demo/valoyes/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/demo/valoyes/notekeeper/NoteCreationTest.java
@@ -1,20 +1,34 @@
 package com.demo.valoyes.notekeeper;
 
+import android.media.MediaPlayer;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.*;
+import static android.support.test.espresso.Espresso.pressBack;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+
+    public static final String NOTE_TITLE = "Test note title";
+    public static final String NOTE_TEXT = "Oye! este el el texte de la nota";
+    static DataManager sDataManager;
+    @BeforeClass
+    public static void setup() throws Exception{
+        sDataManager = DataManager.getInstance();
+    }
 
     // vamos a necesitar una ACTIVITY para ejecutar el test
     // y para ello utilizamos esta clase ActivityTestRule
@@ -28,11 +42,32 @@ public class NoteCreationTest {
 
     @Test
     public void createNewNote(){
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+
+        // 1.
 //        ViewInteraction fabNewNote = onView(withId(R.id.fab));
 //        fabNewNote.perform(click());
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("Oye! este el el texte de la nota"), closeSoftKeyboard());
+
+        // 1.1 buscamos el text_note en el en el spinner, esta accions es necesaria para que funcione
+        // el paso 2, sino se presenta la informacion del spinner mediante un click
+        // el metodo onData no va a funcionar
+        // esta accions no seria necesaria, si por ejemplo estuvieramos con un ListView
+        onView(withId(R.id.spinner_courses)).perform(click());
+
+        // 2.
+        // busca en los AdapterViews de esta activity y encuentra aquel cuya insancia
+        // es igual a nuestra variable "course", para luego ir al spinner y encontrar la View
+        // que corresponde a esta selecci'on
+        // y luego haremos click en el
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+
+        // 3.
+        onView(withId(R.id.text_note_title)).perform(typeText(NOTE_TITLE));
+        onView(withId(R.id.text_note_text)).perform(typeText(NOTE_TEXT), closeSoftKeyboard());
+
+        // 4.
+        pressBack();
     }
 
 }
